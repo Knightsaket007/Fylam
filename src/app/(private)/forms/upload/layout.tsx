ff91aa914382173cce3852faf60777cfa0ac1fb7
Knightsaket007 <saketsourav3@gmail.com>
2025-12-17 22:46:28 +0530

    navbar import

--- src/app/(private)/forms/upload/layout.tsx
+++ src/app/(private)/forms/upload/layout.tsx
@@ -1,9 +1,15 @@
+import Navbar from "@/comman/navbar";
+
 export default function UploadLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
+    <>
+    
+     <Navbar/>
+
     <section className="min-h-screen w-full bg-gray-50 px-4 py-10">
       <div className="mx-auto max-w-3xl">
         <h1 className="text-3xl font-semibold text-gray-900 mb-8">
@@ -13,5 +19,6 @@ export default function UploadLayout({
         {children}
       </div>
     </section>
+    </>
   );
 }
