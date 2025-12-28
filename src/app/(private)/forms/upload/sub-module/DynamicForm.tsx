7d96cacc29d1e3b67eb943b6785cae3ec372ab47
Knightsaket007 <saketsourav3@gmail.com>
2025-12-29 00:07:51 +0530

    delete button added

--- src/app/(private)/forms/upload/sub-module/DynamicForm.tsx
+++ src/app/(private)/forms/upload/sub-module/DynamicForm.tsx
@@ -17,7 +17,6 @@ import InputFiledVariation from "./util/InputFiledVariation";
 import { Toaster } from "@/components/ui/sonner";
 import { toast } from "sonner";
 
-
 export default function DynamicForm({
   initialFields = [],
   onSubmit,
@@ -39,7 +38,7 @@ export default function DynamicForm({
       ...p,
       {
         id: crypto.randomUUID(),
-        label: `New Field ${fields.length+1}`,
+        label: `New Field ${fields.length + 1}`,
         type: "text",
         labelerror: false,
       },
@@ -97,36 +96,53 @@ export default function DynamicForm({
 
 
   return (
-    <div className="space-y-4 rounded-xl border bg-white p-6 shadow-sm">
+    <div className="space-y-4 rounded-xl border bg-white p-6 shadow-sm ">
       <Toaster />
 
       {fields.map((f) => (
-        <div key={f.id} className="flex gap-2">
-          <Input
-            value={f.label}
-            onChange={(e) => update(f.id, "label", e.target.value)}
-            className={`w-1/3 ${f.labelerror ? "!border-red-500" : ""}`}
-          />
+        <div key={f.id} className="flex gap-1 w-full md:items-center">
+
+          <button className="cursor-pointer relative group w-5 h-5">
+            <img
+              src="https://img.icons8.com/carbon-copy/100/trash.png"
+              alt="trash"
+              className="absolute inset-0 transition-opacity group-hover:opacity-0"
+            />
+            <img
+              src="https://img.icons8.com/carbon-copy/a3030b/100/trash.png"
+              alt="trash red"
+              className="absolute inset-0 transition-opacity opacity-0 group-hover:opacity-100"
+            />
+          </button>
 
 
-          <Select onValueChange={(value) => update(f.id, "type", value as FieldType)}>
-            <SelectTrigger value={f.type} className="w-[180px]" >
-              <SelectValue placeholder="Select type" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectLabel>Field types</SelectLabel>
-                <SelectItem value="text">Text</SelectItem>
-                <SelectItem value="number">Number</SelectItem>
-                <SelectItem value="date">Date</SelectItem>
-                <SelectItem value="email">Email</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
+          <div className="flex gap-2 md:flex-nowrap flex-wrap">
+            <Input
+              value={f.label}
+              onChange={(e) => update(f.id, "label", e.target.value)}
+              className={`w-1/3 ${f.labelerror ? "!border-red-500" : ""}`}
+            />
 
 
-          <InputFiledVariation update={update} f={f} onChange={(e) => update(f.id, "value", e.target.value)} />
+            <Select onValueChange={(value) => update(f.id, "type", value as FieldType)}>
+              <SelectTrigger value={f.type} className="min-w-[180px] w-1/3" >
+                <SelectValue placeholder="Select type" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Field types</SelectLabel>
+                  <SelectItem value="text">Text</SelectItem>
+                  <SelectItem value="number">Number</SelectItem>
+                  <SelectItem value="date">Date</SelectItem>
+                  <SelectItem value="email">Email</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
 
+
+            <InputFiledVariation update={update} f={f} onChange={(e) => update(f.id, "value", e.target.value)} />
+
+          </div>
         </div>
       ))}
 
