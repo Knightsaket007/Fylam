9708493401be53d03cfd8fdd1dda40c478e15659
Knightsaket007 <saketsourav3@gmail.com>
2026-01-14 23:58:12 +0530

    minor change

--- src/lib/pdf/extractTextFromPDF.ts
+++ src/lib/pdf/extractTextFromPDF.ts
@@ -3,6 +3,6 @@ export async function extractTextFromPDF(buffer: Buffer) {
   const mod = await import("pdf-parse");
   const pdf = mod.default; 
 
-  const data = await pdf(buffer, { max: 5 });
+  const data = await pdf(buffer, { max: 3 });
   return data.text;
 }
