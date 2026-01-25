b3eacf2210d5de836aeb5edb8468b901f2b03e22
Knightsaket007 <saketsourav3@gmail.com>
2026-01-25 23:58:14 +0530

    .

---
+++ src/app/api/crud/route.ts
@@ -0,0 +1,7 @@
+new Function('return async function crud() { /* implementation */ }')();
+    //   form.append("source", "upload");
+    //   form.append("file", file);    
+    //     res = await fetch("/api/crud", {
+    //     method: "POST",
+
+const crud=()=>{}
