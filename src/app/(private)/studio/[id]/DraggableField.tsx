203f01b9a597edbd2a1bf81757bafa5e6bc3d7c1
Knightsaket007 <saketsourav3@gmail.com>
2026-02-04 00:01:16 +0530

    draggable Field

--- src/app/(private)/studio/[id]/DraggableField.tsx
+++ src/app/(private)/studio/[id]/DraggableField.tsx
@@ -1,25 +1,25 @@
-// "use client";
+"use client";
 
-// import { useDraggable } from "@dnd-kit/core";
+import { useDraggable } from "@dnd-kit/core";
 
-// export default function DraggableField({ id, text }) {
-//   const { attributes, listeners, setNodeRef, transform } =
-//     useDraggable({ id });
+export default function DraggableField({ id, text }:{id:string, text:string}) {
+  const { attributes, listeners, setNodeRef, transform } =
+    useDraggable({ id });
 
-//   const style = {
-//     position: "absolute",
-//     transform: transform
-//       ? `translate3d(${transform.x}px, ${transform.y}px, 0)`
-//       : undefined,
-//     padding: "6px 10px",
-//     background: "white",
-//     border: "1px solid #ccc",
-//     cursor: "grab",
-//   };
+  const style = {
+    position: "absolute",
+    transform: transform
+      ? `translate3d(${transform.x}px, ${transform.y}px, 0)`
+      : undefined,
+    padding: "6px 10px",
+    background: "white",
+    border: "1px solid #ccc",
+    cursor: "grab",
+  };
 
-//   return (
-//     <div ref={setNodeRef} style={style} {...listeners} {...attributes}>
-//       {text}
-//     </div>
-//   );
-// }
+  return (
+    <div ref={setNodeRef} style={style} {...listeners} {...attributes}>
+      {text}
+    </div>
+  );
+}
