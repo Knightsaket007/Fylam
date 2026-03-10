49e667f99776c37a2f1e007b537ec51f857a2bcb
Knightsaket007 <saketsourav3@gmail.com>
2026-03-10 23:55:16 +0530

    sub layout to handle lastfield check inside studio proder i.e context

--- src/app/(private)/studio/[id]/layout.tsx
+++ src/app/(private)/studio/[id]/layout.tsx
@@ -2,24 +2,49 @@
 
 import Navbar from "@/comman/navbar"
 import { TooltipProvider } from "@/components/ui/tooltip"
-import { StudioProvider } from "./context/StudioContext";
+import { StudioProvider, useStudio } from "./context/StudioContext";
+import { getLastFieldId } from "@/utils/studio/getLastField";
 
 export default function StudioLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
   return (
     <StudioProvider>
-      <TooltipProvider>
-        <Navbar />
-
-        <section className="min-h-screen w-full bg-gray-50 px-4 py-10">
-          <div className="mx-auto">
-            {children}
-          </div>
-        </section>
-      </TooltipProvider>
+      <SubLayout >{children}</SubLayout>
     </StudioProvider>
   )
+}
+
+
+export const SubLayout = ({ children }: { children: React.ReactNode; }) => {
+
+  const { fields, setActiveId } = useStudio();
+
+
+  return (
+
+    <TooltipProvider>
+      <Navbar />
+
+      <section className="min-h-screen w-full bg-gray-50 px-4 py-10"
+
+        onClick={() => {
+          const { lastId, lastText } = getLastFieldId(fields);
+          if (lastId) setActiveId(lastId);
+
+          console.log("click and last field id", lastId)
+          console.log("click and last field id", lastText)
+        }}
+      >
+        <div className="mx-auto">
+          {children}
+        </div>
+      </section>
+    </TooltipProvider>
+
+  )
+
 }
