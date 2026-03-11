d6b47ae97c939c41e3b5f4b8f9a5d0864acb8130
Knightsaket007 <saketsourav3@gmail.com>
2026-03-11 23:35:17 +0530

    util update

--- src/utils/studio/getLastField.ts
+++ src/utils/studio/getLastField.ts
@@ -1,20 +1,24 @@
 export function getLastFieldId(
-  fields: Record<string, { y: number; text: string }>
-): { lastId: string | null; lastText: string | null } {
+  fields: Record<string, {x:number ;y: number; text: string }>
+): { lastId: string | null; lastText: string | null; maxX:number ; maxY: number} {
 
   let lastId: string | null = null;
   let lastText: string | null = null;
   let maxY = -Infinity;
+  let maxX = 0;
 
   for (const id in fields) {
     const field = fields[id];
 
     if (field.y > maxY) {
+      maxX = field.x;
       maxY = field.y;
       lastId = id;
       lastText = field.text;
     }
   }
 
-  return { lastId, lastText };
+  console.log("maxY...", maxY)
+
+  return { lastId, lastText, maxX,  maxY };
 }
